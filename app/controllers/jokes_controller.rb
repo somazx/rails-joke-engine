2d30db4534533e3278b931b49567246f9de0a313
Andy <somazx@gmail.com>
2023-11-04 11:26:48 -0700

    Move css and render turbo_stream joke

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,16 +1,15 @@
 class JokesController < ApplicationController
-  layout false
 
   def show
     respond_to do |format|
-      format.turbo_stream do
-        loading = false
-        joke = JokeApi.get_random
-        render ::JokeComponent.new(joke:, loading:)
-      end
-
       format.html do
         render ::JokeComponent.new()
+      end
+
+      format.turbo_stream do
+        joke = JokeApi.get_random
+        loading = false
+        @joke_component = ::JokeComponent.new(joke:, loading:)
       end
     end
   end
