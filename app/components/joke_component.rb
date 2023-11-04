526333e79d3a3ebf9b9bf4897e92a4679820dfc2
Andy <somazx@gmail.com>
2023-11-04 10:34:12 -0700

    Add JokeComponent

--- app/components/joke_component.rb
+++ app/components/joke_component.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class JokeComponent < ViewComponent::Base
-  def initialize(text:)
-    @text = text
+  def initialize(joke: nil, loading: true)
+    @joke = joke
+    @loading = loading
   end
 
+  def loading?
+    @loading
+  end
 end
--- test/components/joke_component_test.rb
+++ test/components/joke_component_test.rb
@@ -3,10 +3,17 @@
 require "test_helper"
 
 class JokeComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
+  def test_component_renders_joke_text
     joke_text = "Is this a joke?"
-    render_inline JokeComponent.new(text: joke_text)
+    render_inline JokeComponent.new(text: joke_text, loading: false)
 
     assert_text(joke_text)
+    refute_selector("svg")
+  end
+
+  def test_component_renders_loading_svg
+    render_inline JokeComponent.new(text: nil, loading: true)
+
+    assert_selector("svg")
   end
 end
